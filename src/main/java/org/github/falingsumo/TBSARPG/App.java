37113d343c666fd719913049efa8cc4e43a63ca6
Sébastien Lalonde <falingsumo@hotmail.com>
2022-09-07 12:38:35 -0400

    Adding basic project structure

--- src/main/java/org/github/falingsumo/TBSARPG/App.java
+++ src/main/java/org/github/falingsumo/TBSARPG/App.java
@@ -5,8 +5,8 @@ import org.github.falingsumo.TBSARPG.graphic.ConsoleGraphicEngine;
 
 public class App {
     public static void main(String[] args) {
-        GameEngine gameEngine = new GameEngine();
         GraphicEngine graphicEngine = new ConsoleGraphicEngine();
+        GameEngine gameEngine = new GameEngine();
         System.out.println("Hello World!");
     }
 }
