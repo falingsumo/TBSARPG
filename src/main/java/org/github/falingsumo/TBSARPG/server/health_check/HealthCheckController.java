365e68c0aa470bc10a1045d2791e0544bffe4887
Sebastien Lalonde <falingsumo@hotmail.com>
2024-02-19 11:50:17 -0500

    Adding response code and response entity

--- src/main/java/org/github/falingsumo/TBSARPG/server/health_check/HealthCheckController.java
+++ src/main/java/org/github/falingsumo/TBSARPG/server/health_check/HealthCheckController.java
@@ -1,5 +1,8 @@
 package org.github.falingsumo.TBSARPG.server.health_check;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("health-check")
 public class HealthCheckController {
 
-    @GetMapping
-    public String healthCheck() {
-        return "testing health-check";
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> healthCheck() {
+        return new ResponseEntity<>("testing health-check", HttpStatus.OK);
     }
 }
