39ffd72789caa49a0e87db4432da636fe6dc1ee6
Sebastien Lalonde <falingsumo@hotmail.com>
2024-02-19 11:39:40 -0500

    Adding base path for API and for health-check controller

--- src/main/java/org/github/falingsumo/TBSARPG/server/Server.java
+++ src/main/java/org/github/falingsumo/TBSARPG/server/Server.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Server {
 	public static void main(String[] args) {
+		System.setProperty("server.servlet.context-path", "/api");
 		SpringApplication.run(Server.class, args);
 	}
 }
--- src/main/java/org/github/falingsumo/TBSARPG/server/health_check/HealthCheckController.java
+++ src/main/java/org/github/falingsumo/TBSARPG/server/health_check/HealthCheckController.java
@@ -1,12 +1,14 @@
 package org.github.falingsumo.TBSARPG.server.health_check;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("health-check")
 public class HealthCheckController {
 
-    @GetMapping("/")
+    @GetMapping
     public String healthCheck() {
         return "testing health-check";
     }
